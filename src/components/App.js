7e1d4cba551077076f8ba77937263dd1935907a6
Asad520 <asadbutt667@gmail.com>
2020-08-29 16:40:42 +0500

    stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
+import Head from "../components/Head";
+import imag from "../imag/yui.jpg";
 
-import Head from "./Head";
-import Main from "./Main";
-import Foot from "./Foot";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { isLoading: true, loggedIn: false };
+    this.logStatus = this.logStatus.bind(this);
+  }
 
-function App() {
-  return (
-    <div>
-      <Head />
-      <Main />
-      <Foot />
-    </div>
-  );
+  logStatus(status) {
+    console.log("AAAAAAAAAAAAAAAAA", status);
+    this.setState((prevState) => {
+      return { loggedIn: !prevState.loggedIn };
+    });
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div>
+            <Head
+              name="Asad"
+              status={this.state.loggedIn}
+              logStatus={this.logStatus}
+            />
+            <Main />
+            <Foot />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main">
+        <ul>
+          This is main site:
+          <li>Option 1 </li>
+          <li>Option 1 </li>
+          <li>Option 1 </li>
+        </ul>
+        <img
+          src={imag}
+          alt="No Pic Found"
+          onMouseOver={() =>
+            (document.getElementsByClassName("yui")[0].innerHTML =
+              "YYYYUUUUIIIII!!!!")
+          }
+          onMouseLeave={() =>
+            (document.getElementsByClassName("yui")[0].innerHTML = "")
+          }
+        />
+        <div className="yui"></div>
+      </div>
+    );
+  }
+}
+
+function Foot() {
+  return <h3 className="foot">This is my footer!!!</h3>;
 }
 
 export default App;
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Head() {
-  return <h1 className="navBar">This is my header!!!</h1>;
+function Head(props) {
+  let buttonText = props.status ? "Log Out" : "Log In";
+  return (
+    <div>
+      {props.status ? (
+        <h1 className="navBar">Welcome {props.name}! This is my header!!!</h1>
+      ) : (
+        <h1>Kindly Login to continue!</h1>
+      )}
+      <button onClick={() => props.logStatus(props.status)}>
+        {buttonText}
+      </button>
+    </div>
+  );
 }
 
 export default Head;
